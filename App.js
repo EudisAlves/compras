42514c4d1e8a0dfba82ae5d9343c8be087c9c7a2
Eudis Alves <111400440+EudisAlves@users.noreply.github.com>
2023-04-25 18:53:34 -0300

    Criando componentes

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Title from './components/Title';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Calcule seu imac!</Text>
-      <StatusBar style="auto" />
+      <Title />
     </View>
   );
 }
